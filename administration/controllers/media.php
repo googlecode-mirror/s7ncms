39d5f7b8c732b6cec38d4bb74f6d2aabfb0a0aa2
edy-b <edy-b@3759754b-2546-0410-a4b9-f53f42733e87>
2007-11-27 19:38:52 +0000

    admin media controller updated to prevent files not being loaded from browser cache

--- administration/controllers/media.php
+++ administration/controllers/media.php
@@ -5,14 +5,33 @@ class Media_Controller extends Controller {
 	public function _remap() {
 		$type = $this->uri->segment(2);
 		$file = $this->uri->segment(3);
-
+        $ext = substr(strrchr($file, '.'), 1 );
+        
 		$this->auto_render = false;
 		
-		try {
+        $path = Kohana::find_file('views', 'media/'.$type.'/'.$file, FALSE, $ext);
+        
+        if(file_exists($path)) {
+		
+		    $time_file = filemtime(Kohana::find_file('views', 'media/'.$type.'/'.$file, FALSE, $ext));
+            
+            if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {    		    
+                $time_cache = strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
+                
+                if ($time_file <= $time_cache) {
+                    header('HTTP/1.1 304 Not Modified');
+                    exit;
+                }
+            }
+            
+            $header = 'Last-Modified: ';
+            $header .= gmdate('D, d M Y H:i:s', $time_file);
+            $header .= ' GMT';
+            header($header);
+            
 			echo new View('media/'.$type.'/'.$file);
-		} catch (Kohana_Exception $e) {
-			throw new Kohana_404_Exception();
+		} else {
+			Event::run('system.404');
 		}
-	}
-	
+	}	
 }
