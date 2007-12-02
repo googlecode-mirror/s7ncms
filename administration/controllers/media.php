287b6d6c065ef9276c64b7581575105cfe523916
edy-b <edy-b@3759754b-2546-0410-a4b9-f53f42733e87>
2007-12-02 19:16:01 +0000

    cleaned up media controller code

--- administration/controllers/media.php
+++ administration/controllers/media.php
@@ -3,33 +3,32 @@
 class Media_Controller extends Controller {
 
 	public function _remap() {
-		$type = $this->uri->segment(2);
-		$file = $this->uri->segment(3);
-        $ext = substr(strrchr($file, '.'), 1 );
-        
+		$file = $this->uri->string();
+		$ext = substr(strrchr($file, '.'), 1 );
+
 		$this->auto_render = false;
-		
-        $path = Kohana::find_file('views', 'media/'.$type.'/'.$file, FALSE, $ext);
-        
-        if(file_exists($path)) {
-		
-		    $time_file = filemtime($path);
-            
-            if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {    		    
-                $time_cache = strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
-                
-                if ($time_file <= $time_cache) {
-                    header('HTTP/1.1 304 Not Modified');
-                    exit;
-                }
-            }
-            
-            $header = 'Last-Modified: ';
-            $header .= gmdate('D, d M Y H:i:s', $time_file);
-            $header .= ' GMT';
-            header($header);
-            
-			echo new View('media/'.$type.'/'.$file);
+
+		$path = Kohana::find_file('views', $file, FALSE, $ext);
+
+		if(file_exists($path)) {
+
+			$time_file = filemtime($path);
+
+			if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+				$time_cache = strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
+
+				if ($time_file <= $time_cache) {
+					header('HTTP/1.1 304 Not Modified');
+					exit;
+				}
+			}
+
+			$header = 'Last-Modified: ';
+			$header .= gmdate('D, d M Y H:i:s', $time_file);
+			$header .= ' GMT';
+			header($header);
+
+			echo new View($file);
 		} else {
 			Event::run('system.404');
 		}
