385b89a54624643eb0debe71e95b4586dcdaa200
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2008-04-11 21:48:59 +0000

    little optimizations

--- application/models/page.php
+++ application/models/page.php
@@ -2,6 +2,19 @@
 
 class Page_Model extends ORM {
 	protected $belongs_to = array('user');
+	
+	/**
+	 * Allows Pages to be loaded by id or uri title.
+	 */
+	protected function where_key($id = NULL)
+	{
+		if(! ctype_digit($id))
+		{
+			return 'uri';
+		}
+		
+		return parent::where_key($id);
+	}
 }
 
 /*class Pages_Model extends Model {
