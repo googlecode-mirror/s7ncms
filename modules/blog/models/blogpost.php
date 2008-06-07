c893dac5325202a5d7f8c5df87ef569a257142c5
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2008-06-07 10:00:56 +0000

    added (c) info

--- modules/blog/models/blogpost.php
+++ modules/blog/models/blogpost.php
@@ -1,5 +1,16 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
+/**
+ * S7Ncms - www.s7n.de
+ *
+ * Copyright (c) 2007-2008, Eduard Baun <eduard at baun.de>
+ * All rights reserved.
+ *
+ * See license.txt for full text and disclaimer
+ *
+ * @author Eduard Baun <eduard at baun.de>
+ * @copyright Eduard Baun, 2007-2008
+ * @version $Id$
+ */
 class Blogpost_Model extends ORM {
 	
 	protected $has_many = array('comments');
