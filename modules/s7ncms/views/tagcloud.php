2cfb67501ccd25cebf09d777156f8cf69bbbb6e3
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2008-07-22 19:46:15 +0000

    we need here to rawurlencode the tagnames

--- modules/s7ncms/views/tagcloud.php
+++ modules/s7ncms/views/tagcloud.php
@@ -1,5 +1,5 @@
 <div id="tagcloud">
 <?php foreach ($tags as $tag): ?>
-	<span style="font-size:<?php echo $tag['size'] ?>%;"><?php echo html::anchor($tag['uri'], $tag['name']) ?></span>
+	<span style="font-size:<?php echo $tag['size'] ?>%;"><?php echo html::anchor($tag['uri'], rawurlencode($tag['name'])) ?></span>
 <?php endforeach ?>
 </div>
