9641b6cf49ae53e0d1b4c784e66c8e731f697b4c
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2008-10-02 20:13:44 +0000

    you need this file too

---
+++ modules/s7ncms/libraries/MY_Router.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * S7Ncms - www.s7n.de
+ *
+ * Copyright (c) 2007-2008, Eduard Baun <eduard at baun.de>
+ * All rights reserved.
+ *
+ * See license.txt for full text and disclaimer
+ *
+ * @author Eduard Baun <eduard at baun.de>
+ * @copyright Eduard Baun, 2007-2008
+ * @version $Id$
+ */
+class Router extends Router_Core {
+	static $routed_uri;
+	static $current_id;
+}
