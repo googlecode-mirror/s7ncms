b9a17d1b5a995d8556cb5d025fb07498283be7b3
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2009-03-04 13:52:49 +0000

    updated index.php (see http://dev.kohanaphp.com/changeset/3915)

--- index.php
+++ index.php
@@ -5,16 +5,48 @@
  * module demonstration controllers will result in 404 errors. For more information
  * about this option, read the documentation about deploying Kohana.
  *
- * @see http://doc.kohanaphp.com/installation/deployment
+ * @see http://docs.kohanaphp.com/installation/deployment
  */
 define('IN_PRODUCTION', FALSE);
 
 /**
- * Upload directory.
+ * Website application directory. This directory should contain your application
+ * configuration, controllers, models, views, and other resources.
  *
  * This path can be absolute or relative to this file.
  */
+$kohana_application = 'application';
+
+/**
+ * Kohana modules directory. This directory should contain all the modules used
+ * by your application. Modules are enabled and disabled by the application
+ * configuration file.
+ *
+ * This path can be absolute or relative to this file.
+ */
+$kohana_modules = 'modules';
+
+/**
+ * Kohana system directory. This directory should contain the core/ directory,
+ * and the resources you included in your download of Kohana.
+ *
+ * This path can be absolute or relative to this file.
+ */
+$kohana_system = 'system';
+
+/**
+ * S7Ncms upload directory. This directory contains all uploaded files
+ * 
+ * This path can be absolute or relative to this file.
+ */
 $s7n_upload = 'upload';
+
+/**
+ * S7Ncms themes directory. This directory contains themes used in S7Ncms
+ * 
+ * This path can be absolute or relative to this file.
+ */
+$s7n_themes = 'themes';
 
 /**
  * Test to make sure that Kohana is running on PHP 5.2 or newer. Once you are
@@ -38,7 +70,7 @@ ini_set('display_errors', TRUE);
 
 /**
  * If you rename all of your .php files to a different extension, set the new
- * extension here. This option can left to .php, even if this file is has a
+ * extension here. This option can left to .php, even if this file has a
  * different extension.
  */
 define('EXT', '.php');
@@ -49,19 +81,30 @@ define('EXT', '.php');
 // $Id$
 //
 
+$kohana_pathinfo = pathinfo(__FILE__);
 // Define the front controller name and docroot
-define('DOCROOT', getcwd().DIRECTORY_SEPARATOR);
-define('KOHANA',  basename(__FILE__));
+define('DOCROOT', $kohana_pathinfo['dirname'].DIRECTORY_SEPARATOR);
+define('KOHANA',  $kohana_pathinfo['basename']);
 
 // If the front controller is a symlink, change to the real docroot
 is_link(KOHANA) and chdir(dirname(realpath(__FILE__)));
 
-// Define application and system paths
-define('APPPATH', str_replace('\\', '/', realpath('application')).'/');
-define('MODPATH', str_replace('\\', '/', realpath('modules')).'/');
-define('SYSPATH', str_replace('\\', '/', realpath('system')).'/');
-define('THEMEPATH', str_replace('\\', '/', realpath('themes')).'/');
-define('UPLOADPATH', str_replace('\\', '/', realpath('upload')).'/');
+// If kohana folders are relative paths, make them absolute.
+$kohana_application = file_exists($kohana_application) ? $kohana_application : DOCROOT.$kohana_application;
+$kohana_modules = file_exists($kohana_modules) ? $kohana_modules : DOCROOT.$kohana_modules;
+$kohana_system = file_exists($kohana_system) ? $kohana_system : DOCROOT.$kohana_system;
+$s7n_upload = file_exists($s7n_upload) ? $s7n_upload : DOCROOT.$s7n_upload;
+$s7n_themes = file_exists($s7n_themes) ? $s7n_themes : DOCROOT.$s7n_themes;
 
-// Initialize.
+// Define application and system paths
+define('APPPATH', str_replace('\\', '/', realpath($kohana_application)).'/');
+define('MODPATH', str_replace('\\', '/', realpath($kohana_modules)).'/');
+define('SYSPATH', str_replace('\\', '/', realpath($kohana_system)).'/');
+define('UPLOADPATH', str_replace('\\', '/', realpath($s7n_upload)).'/');
+define('THEMEPATH', str_replace('\\', '/', realpath($s7n_themes)).'/');
+
+// Clean up
+unset($kohana_application, $kohana_modules, $kohana_system, $s7n_upload, $s7n_themes);
+
+// Initialize Kohana
 require SYSPATH.'core/Bootstrap'.EXT;
