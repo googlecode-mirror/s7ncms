fec85c496fd15835a8d1e90ccaa8733c4e019bdd
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2009-04-25 22:33:30 +0000

    refactoring

--- install/controllers/install.php
+++ install/controllers/install.php
@@ -13,89 +13,69 @@
  */
 class Install_Controller extends Template_Controller {
 
-    public function index()
-    {
-        $dbuser='';
-        $dbpass='';
-        $dbhost='';
-        $dbdatabase='';
-        if($_POST)
-        {
-            echo Kohana::debug($this->input->post());
-            $dbuser=$_POST['dbuser'];
-            $dbpass=$_POST['dbpass'];
-            $dbhost=$_POST['dbhost'];
-            $dbdatabase= $_POST['dbdatabase'];
-        }
-            
-            $this->template->dberror="";
-            
-            $config = array
-            (
-                'benchmark'     => FALSE,
-                'persistent'    => FALSE,
-                'connection'    => array
-                (
-                    'type'     => 'mysql',
-                    'user'     => 'bla',
-                    'pass'     => 'fsadfsadf',
-                    'host'     => 'jo',
-                    'port'     => FALSE,
-                    'socket'   => FALSE,
-                    'database' => 'test'
-                ),
-                'character_set' => 'utf8',
-                'table_prefix'  => '<?php echo $table_prefix ?>',
-                'object'        => TRUE,
-                'cache'         => TRUE,
-                'escape'        => TRUE
-            );
-            
-            try
-            {
-                $this->check_mysql($dbhost, $dbdatabase, $dbuser, $dbpass);
-                
-                echo "db ok";
-                // weiter gehts, alles ok
-            } 
-            catch (Exception $e)
-            {
-                echo "db not ok";
-                $this->template->dberror =$e->getMessage();
-            }
+	public function index()
+	{
+		// TODO check
+	}
+	
+	public function step_database()
+	{
+		$dbuser='';
+		$dbpass='';
+		$dbhost='';
+		$dbdatabase='';
 
-    }
-    
-    
-    private function check_mysql($host, $database, $username, $password) {
-                $link = @mysql_connect($host, $username, $password);
-                if(!$link) {
-                        $error = '';
-                        
-                        if(strpos(mysql_error(), 'Access denied') !== false) {
-                                $error = 'wrong username or password';
-                        } elseif(strpos(mysql_error(), 'server host') !== false) {
-                                $error = 'unknown host: '.$host;
-                        } elseif(strpos(mysql_error(), 'connect to') !== false) {
-                                $error = 'Can\'t connect to host: '.$host;
-                        }else {
-                                $error = mysql_error();
-                        }
-                        
-                        throw new Exception($error);
-                        return $error;
-                }
-                
-                $select = mysql_select_db($database, $link);
-                if (!$select) {
-                    throw new Exception(mysql_error());
-                        return mysql_error();
-                }
-                
-                return true;
-        }
+		if($_POST)
+		{
+			$dbuser=$_POST['dbuser'];
+			$dbpass=$_POST['dbpass'];
+			$dbhost=$_POST['dbhost'];
+			$dbdatabase= $_POST['dbdatabase'];
+		}
+			
+		$this->template->dberror="";
+		
+		try
+		{
+			$this->check_mysql($dbhost, $dbdatabase, $dbuser, $dbpass);
+			
+			echo "db ok";
+			// weiter gehts, alles ok
+		} 
+		catch (Exception $e)
+		{
+			echo "db not ok";
+			$this->template->dberror =$e->getMessage();
+		}
+	}
+	
+	
+	private function check_mysql($host, $database, $username, $password) {
+		$link = @mysql_connect($host, $username, $password);
+		if(!$link) {
+				$error = '';
+				
+				if(strpos(mysql_error(), 'Access denied') !== false) {
+						$error = 'wrong username or password';
+				} elseif(strpos(mysql_error(), 'server host') !== false) {
+						$error = 'unknown host: '.$host;
+				} elseif(strpos(mysql_error(), 'connect to') !== false) {
+						$error = 'Can\'t connect to host: '.$host;
+				}else {
+						$error = mysql_error();
+				}
+				
+				throw new Exception($error);
+				return $error;
+		}
+		
+		$select = mysql_select_db($database, $link);
+		if (!$select) {
+			throw new Exception(mysql_error());
+				return mysql_error();
+		}
+		
+		return true;
+	}
 
 }
-
-
-
