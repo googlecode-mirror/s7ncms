021744b006fce294c14bc3e6bb0ea0225ce8cbeb
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2009-05-15 13:24:04 +0000

    added fancybox module. writing modules for S7Ncms is fun!

---
+++ modules/fancybox/hooks/fancybox.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * S7Ncms - www.s7n.de
+ *
+ * Copyright (c) 2007-2009, Eduard Baun <eduard at baun.de>
+ * All rights reserved.
+ *
+ * See license.txt for full text and disclaimer
+ *
+ * @author Eduard Baun <eduard at baun.de>
+ * @copyright Eduard Baun, 2007-2009
+ * @version $Id$
+ */
+
+Event::add('s7n.theme.head', 'fancybox::head');
+Event::add('s7n.display_content', 'fancybox::replace_content');
