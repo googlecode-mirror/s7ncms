3e74329b21d49a4ff4481d44c55cf86c4b515daa
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2009-05-16 17:45:25 +0000

    removed page caching. need something better

--- modules/s7ncms/controllers/website.php
+++ modules/s7ncms/controllers/website.php
@@ -19,11 +19,6 @@ class Website_Controller extends Template_Controller {
 	{
 		parent::__construct();
 
-		if (IN_PRODUCTION === TRUE)
-		{
-			Event::add('system.display', array($this, 'save_cache'));
-		}
-
 		$this->session = Session::instance();
 		$this->head = Head::instance();
 		$this->head->css->append_file('themes/'.config::get('s7n.theme').'/css/layout');
@@ -31,14 +26,6 @@ class Website_Controller extends Template_Controller {
 
 		$this->template->set_global('theme_url', 'themes/'.config::get('s7n.theme').'/');
 		$this->template->head = $this->head;
-	}
-
-	public function save_cache()
-	{
-		if ($this->cache_enabled === TRUE)
-		{
-			Cache::instance()->set('s7n_'.Router::$current_uri, Event::$data);
-		}
 	}
 
 }
