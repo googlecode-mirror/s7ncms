c26d8ed8c05d8c4f590c909639fa1c9b9bd28358
oliver.morgan <oliver.morgan@freshinc.co.uk>
2009-05-30 19:42:13 +0000

    Final database revision, including full UTF8 support.

--- dump.sql
+++ dump.sql
@@ -2,45 +2,44 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `mydb`;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`languages`
+-- Table `languages`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`languages` (
+CREATE  TABLE IF NOT EXISTS `languages` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `tag` VARCHAR(6) NOT NULL ,
   `name` VARCHAR(45) NOT NULL ,
   `default` BOOLEAN NOT NULL ,
   `sample` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`nations`
+-- Table `nations`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`nations` (
+CREATE  TABLE IF NOT EXISTS `nations` (
   `id` INT UNSIGNED NOT NULL ,
   `tag` VARCHAR(6) NOT NULL ,
   `name` VARCHAR(45) NOT NULL ,
   `timezone` TIMESTAMP NOT NULL ,
-  `language_id` INT NULL ,
+  `language_id` INT UNSIGNED NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_nations_languages` (`language_id` ASC) ,
   CONSTRAINT `fk_nations_languages`
     FOREIGN KEY (`language_id` )
-    REFERENCES `mydb`.`languages` (`id` )
+    REFERENCES `languages` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `users`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`users` (
+CREATE  TABLE IF NOT EXISTS `users` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `username` VARCHAR(45) NOT NULL ,
   `password` VARCHAR(45) NOT NULL ,
@@ -50,21 +49,21 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`users` (
   `last_login` DATETIME NOT NULL ,
   `created` DATETIME NOT NULL ,
   `auth_code` VARCHAR(45) NULL ,
-  `nation_id` INT NULL ,
+  `nation_id` INT UNSIGNED NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_users_nations` (`nation_id` ASC) ,
   CONSTRAINT `fk_users_nations`
     FOREIGN KEY (`nation_id` )
-    REFERENCES `mydb`.`nations` (`id` )
+    REFERENCES `nations` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`pages`
+-- Table `pages`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`pages` (
+CREATE  TABLE IF NOT EXISTS `pages` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `author_id` INT UNSIGNED NOT NULL ,
   `access` INT NOT NULL ,
@@ -77,16 +76,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`pages` (
   INDEX `fk_pages_users` (`author_id` ASC) ,
   CONSTRAINT `fk_pages_users`
     FOREIGN KEY (`author_id` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`modules`
+-- Table `modules`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`modules` (
+CREATE  TABLE IF NOT EXISTS `modules` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
   `description` VARCHAR(255) NOT NULL ,
@@ -94,25 +93,25 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`modules` (
   `enabled` BOOLEAN NOT NULL ,
   `access` INT NOT NULL ,
   PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`roles`
+-- Table `roles`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`roles` (
+CREATE  TABLE IF NOT EXISTS `roles` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `access` INT UNSIGNED NOT NULL ,
   `name` VARCHAR(45) NOT NULL ,
   `description` VARCHAR(255) NULL ,
   PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`users_roles`
+-- Table `users_roles`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`users_roles` (
+CREATE  TABLE IF NOT EXISTS `users_roles` (
   `user_id` INT UNSIGNED NOT NULL ,
   `role_id` INT UNSIGNED NOT NULL ,
   INDEX `fk_users_roles_users` (`user_id` ASC) ,
@@ -120,21 +119,21 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`users_roles` (
   PRIMARY KEY (`user_id`, `role_id`) ,
   CONSTRAINT `fk_users_roles_users`
     FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_users_roles_roles`
     FOREIGN KEY (`role_id` )
-    REFERENCES `mydb`.`roles` (`id` )
+    REFERENCES `roles` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`menus`
+-- Table `menus`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`menus` (
+CREATE  TABLE IF NOT EXISTS `menus` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `pages_id` INT UNSIGNED NOT NULL ,
   `name` VARCHAR(45) NOT NULL ,
@@ -144,33 +143,33 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`menus` (
   INDEX `fk_menus_pages` (`pages_id` ASC) ,
   CONSTRAINT `fk_menus_menus`
     FOREIGN KEY (`parent_id` )
-    REFERENCES `mydb`.`menus` (`id` )
+    REFERENCES `menus` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_menus_pages`
     FOREIGN KEY (`pages_id` )
-    REFERENCES `mydb`.`pages` (`id` )
+    REFERENCES `pages` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`blocks`
+-- Table `blocks`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`blocks` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `blocks` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `access` INT NOT NULL ,
   `enabled` BOOLEAN NOT NULL ,
   `weight` INT NOT NULL ,
   PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`content_type`
+-- Table `content_type`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`content_type` (
+CREATE  TABLE IF NOT EXISTS `content_type` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `page_id` INT UNSIGNED NULL ,
   `block_id` INT UNSIGNED NULL ,
@@ -179,22 +178,22 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`content_type` (
   INDEX `fk_content_join_blocks` (`block_id` ASC) ,
   CONSTRAINT `fk_content_join_pages`
     FOREIGN KEY (`page_id` )
-    REFERENCES `mydb`.`pages` (`id` )
+    REFERENCES `pages` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_content_join_blocks`
     FOREIGN KEY (`block_id` )
-    REFERENCES `mydb`.`blocks` (`id` )
+    REFERENCES `blocks` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`revisions`
+-- Table `revisions`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`revisions` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
+CREATE  TABLE IF NOT EXISTS `revisions` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `user_id` INT UNSIGNED NOT NULL ,
   `date` DATETIME NOT NULL ,
   `active` BOOLEAN NOT NULL ,
@@ -203,16 +202,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`revisions` (
   INDEX `fk_revisions_users` (`user_id` ASC) ,
   CONSTRAINT `fk_revisions_users`
     FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`users` (`id` )
+    REFERENCES `users` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`content`
+-- Table `content`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`content` (
+CREATE  TABLE IF NOT EXISTS `content` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `language_id` INT UNSIGNED NOT NULL ,
   `content_type_id` INT UNSIGNED NOT NULL ,
@@ -228,26 +227,26 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`content` (
   INDEX `fk_content_revisions` (`revision_id` ASC) ,
   CONSTRAINT `fk_content_languages`
     FOREIGN KEY (`language_id` )
-    REFERENCES `mydb`.`languages` (`id` )
+    REFERENCES `languages` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_content_content_type`
     FOREIGN KEY (`content_type_id` )
-    REFERENCES `mydb`.`content_type` (`id` )
+    REFERENCES `content_type` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_content_revisions`
     FOREIGN KEY (`revision_id` )
-    REFERENCES `mydb`.`revisions` (`id` )
+    REFERENCES `revisions` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`configs`
+-- Table `configs`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`configs` (
+CREATE  TABLE IF NOT EXISTS `configs` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `key` TEXT NOT NULL ,
   `modules_id` INT NOT NULL ,
@@ -255,16 +254,16 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`configs` (
   INDEX `fk_config_modules` (`modules_id` ASC) ,
   CONSTRAINT `fk_config_modules`
     FOREIGN KEY (`modules_id` )
-    REFERENCES `mydb`.`modules` (`id` )
+    REFERENCES `modules` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`page_blocks`
+-- Table `page_blocks`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`page_blocks` (
+CREATE  TABLE IF NOT EXISTS `page_blocks` (
   `page_id` INT UNSIGNED NOT NULL ,
   `block_id` INT UNSIGNED NOT NULL ,
   PRIMARY KEY (`page_id`, `block_id`) ,
@@ -272,38 +271,38 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`page_blocks` (
   INDEX `fk_page_blocks_blocks` (`block_id` ASC) ,
   CONSTRAINT `fk_page_blocks_pages`
     FOREIGN KEY (`page_id` )
-    REFERENCES `mydb`.`pages` (`id` )
+    REFERENCES `pages` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_page_blocks_blocks`
     FOREIGN KEY (`block_id` )
-    REFERENCES `mydb`.`blocks` (`id` )
+    REFERENCES `blocks` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`keywords`
+-- Table `keywords`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`keywords` (
-  `id` INT NOT NULL ,
+CREATE  TABLE IF NOT EXISTS `keywords` (
+  `id` INT UNSIGNED NOT NULL ,
   `content_id` INT UNSIGNED NULL ,
   `value` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_keywords_content` (`content_id` ASC) ,
   CONSTRAINT `fk_keywords_content`
     FOREIGN KEY (`content_id` )
-    REFERENCES `mydb`.`content` (`id` )
+    REFERENCES `content` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`config_languages`
+-- Table `config_languages`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`config_languages` (
+CREATE  TABLE IF NOT EXISTS `config_languages` (
   `language_id` INT UNSIGNED NOT NULL ,
   `config_id` INT UNSIGNED NOT NULL ,
   `value` TEXT NOT NULL ,
@@ -312,16 +311,15 @@ CREATE  TABLE IF NOT EXISTS `mydb`.`config_languages` (
   PRIMARY KEY (`language_id`, `config_id`) ,
   CONSTRAINT `fk_config_languages_languages`
     FOREIGN KEY (`language_id` )
-    REFERENCES `mydb`.`languages` (`id` )
+    REFERENCES `languages` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_config_languages_config`
     FOREIGN KEY (`config_id` )
-    REFERENCES `mydb`.`configs` (`id` )
+    REFERENCES `configs` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+ENGINE = InnoDB DEFAULT CHARACTER SET utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
