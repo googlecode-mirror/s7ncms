27c3ed652091924d27717d86f665391472acd06e
eduardbaun <eduardbaun@3759754b-2546-0410-a4b9-f53f42733e87>
2009-06-02 18:56:13 +0000

    improved page cache

--- core/models/page.php
+++ core/models/page.php
@@ -16,41 +16,46 @@ class Page_Model extends ORM {
 
 	protected $has_many = array('contents' => 'content_types');
 	protected $belongs_to = array('author' => 'user');
-	private static $cache = array();
 
 	public function content($language = FALSE)
 	{
 		$lang = $language ? language::id($language) : language::$id;
-		
-		if (isset(self::$cache[$lang][$this->id]))
-			return self::$cache[$lang][$this->id];
 
-		return self::$cache[$lang][$this->id] = ORM::factory('page', $this->id)->where(array('language_id' => $lang))->contents->current();
+		$cache_name = sha1('page_content' . $this->id . $lang);
+
+		if (($content = Cache::instance()->get($cache_name)) === NULL)
+		{
+			$content = ORM::factory('page', $this->id)->where(array('language_id' => $lang))->contents->current();
+
+			Cache::instance()->set($cache_name, $content, array('page'));
+		}
+
+		return $content;
 	}
 
 	public function uri($language = FALSE)
 	{
 		$cache_name = sha1('page_uri' . $this->id . ($language ? $language : language::$tag));
-		
-		if (($cache = Cache::instance()->get($cache_name)) !== NULL)
-			return $cache;
-		
-		$menu_item = ORM::factory('menu')->where('page_id', $this->id)->find();
 
-		$parents = $menu_item->parents()->find_all();
+		if (($uri = Cache::instance()->get($cache_name)) === NULL)
+		{
+			$menu_item = ORM::factory('menu')->where('page_id', $this->id)->find();
 
-		$uri = array();
-		foreach ($parents as $parent)
-			if ($parent->lvl !== 0)
-				$uri[] = ORM::factory('page', $parent->id)->content($language)->uri;
+			$parents = $menu_item->parents()->find_all();
 
-		if ($menu_item->lvl !== 0)
-			$uri[] = $this->content($language)->uri;
-			
-		$uri = implode('/', $uri).'/';
-		
-		Cache::instance()->set($cache_name, $uri, array('page'));
-		
+			$uri = array();
+			foreach ($parents as $parent)
+				if ($parent->lvl !== 0)
+					$uri[] = ORM::factory('page', $parent->id)->content($language)->uri;
+
+			if ($menu_item->lvl !== 0)
+				$uri[] = $this->content($language)->uri;
+
+			$uri = implode('/', $uri).'/';
+
+			Cache::instance()->set($cache_name, $uri, array('page'));
+		}
+
 		return $uri;
 	}
 
